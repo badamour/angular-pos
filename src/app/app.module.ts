b8be06b92c6a2d2a6bacc5a7a203c024c44a6eb6
chamod <chamodsupunsaranayanashan@gmail.com>
2019-06-18 11:32:21 +0530

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './view/dashboard/dashboard.component';
+import {CustomersComponent} from './view/customers/customers.component';
+import {ItemsComponent} from './view/items/items.component';
+import {OrdersComponent} from './view/orders/orders.component';
+import {SlidebarComponent} from './view/slidebar/slidebar.component';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CustomersComponent,
+    ItemsComponent,
+    OrdersComponent,
+    SlidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
