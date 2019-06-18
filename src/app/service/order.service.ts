052271bb3c79a1af44cbf6685ad88fe65f5aac2a
chamod <chamodsupunsaranayanashan@gmail.com>
2019-06-18 10:11:19 +0530

    create order and orderdetail services

---
+++ src/app/service/order.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Order} from '../dto/order';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  readonly baseUrl = environment.apiUrl + '/order';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllOrders(): Observable<Order[]> {
+    return this.http.get<Order[]>('http://localhost:8080/auth/orders');
+  }
+
+  saveOrder(order: Order): Observable<boolean> {
+    return this.http.post<boolean>(this.baseUrl, order);
+  }
+}
---
+++ src/app/service/orderDetail.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {OrderDetail} from '../dto/orderDetail';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+
+  readonly baseUrl = environment.apiUrl + '/orderDetail';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAllOrderDetails(): Observable<OrderDetail[]> {
+    return this.http.get<OrderDetail[]>('http://localhost:8080/auth/orderDetail');
+  }
+
+  saveOrderDetails(orderDetails: OrderDetail): Observable<boolean> {
+    return this.http.post<boolean>(this.baseUrl, orderDetails);
+  }
+}
