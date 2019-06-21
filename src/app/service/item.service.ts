414ac921714d2294e388fe39487aa3023ab7e54c
chamod <chamodsupunsaranayanashan@gmail.com>
2019-06-21 08:55:14 +0530

    change item service methods

--- src/app/service/item.service.ts
+++ src/app/service/item.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Item} from '../dto/item';
 import {environment} from '../../environments/environment';
 
 @Injectable({
@@ -10,5 +12,13 @@ export class ItemService {
   readonly baseUrl = environment.apiUrl + '/items';
 
   constructor(private http: HttpClient) {
+  }
+
+  getAllItems(): Observable<Item[]> {
+    return this.http.get<Item[]>('http://localhost:8080/auth/items');
+  }
+
+  saveItem(item: Item): Observable<boolean> {
+    return this.http.post<boolean>(this.baseUrl, item);
   }
 }
